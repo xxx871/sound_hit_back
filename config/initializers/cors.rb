9e3cd9da74825922435e964781b2711157c9065e
xxx871 <0kh38c317264.7u@gmail.com>
2024-04-25 17:40:01 +0900

    chore: fly.ioへのデプロイ

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8000', '127.0.0.1:8000', 'http://host.docker.internal:3000/posts'
+    origins 'localhost:8000', '127.0.0.1:8000', 'http://host.docker.internal:3000/', 'https://sound-hit-back.fly.dev/', 'https://sound-hit.vercel.app'
 
     resource "*",
       headers: :any,
