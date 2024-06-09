78ab1c43b1e88e52b5807a920f93f705b0e84684
xxx871 <0kh38c317264.7u@gmail.com>
2024-06-09 23:56:18 +0900

    サインアップ機能実装

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
   private
 
-    def sign_up_params
-      params.permit(:email, :password, :password_confirmation, :name)
-    end
+  def sign_up_params
+    params.permit(:email, :password, :password_confirmation, :name)
+  end
+
+  def render_error
+    render json: {
+      status: 'error',
+      message: resource.errors.full_messages.first
+    }, status: :unprocessable_entity
+  end
 end
